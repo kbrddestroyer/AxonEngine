e4fc8d35437266cecf8b68df0b4a6680a254c281
Keyboard Destroyer <grudko2003@gmail.com>
2025-05-28 23:51:28 +0300

    Pref draft

---
+++ tests/performance/perf_serialization.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <iostream>
+
+#include <iomanip>
+#include <bitset>
+#include <serialization/serialization.hpp>
+
+
+int main (int __argc, char* __argv[]) {
+    const char buffer[] = "Hello World!";
+
+    const std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+    size64_t total;
+    const char* result = serialize(buffer, sizeof(buffer), 2, &total);
+
+    const std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+    std::cout <<
+            std::left << std::setw(50) << "Execution time: " <<
+                std::chrono::duration_cast<std::chrono::nanoseconds> ( end - start ).count() << "ns" << std::endl <<
+                    std::setw(50) << "Serialization allocated " << total << " bytes of memory"
+    << std::endl;
+
+    for (size64_t i = 0; i < total; i++) {
+        std::cout << std::setw(15) << std::bitset<8>( result[i] ) << std::setw(5) << result[i] << std::setw(5) << static_cast<uint16_t>(result[i]) << std::endl;
+    }
+
+    return 0;
+}
---
+++ tests/performance/perftest.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <chrono>
+#define PERF_INIT time_t timestamp = 0;
+#define PERF_START  \
+    timestamp = std::chrono::
