98d14db8e4b9d8613d0ccdb93881d9e5191daf26
Keyboard Destroyer <grudko2003@gmail.com>
2025-06-01 16:21:11 +0300

    Fix build

--- libraries/backends/berkeley/tcp/tcp_connection.h
+++ libraries/backends/berkeley/tcp/tcp_connection.h
@@ -9,7 +9,7 @@ uint8_t connect_tcp_client(SOCKADDR_IN_T *, SOCKET_T *, const char *, uint32_t);
 uint8_t create_tcp_server(SOCKADDR_IN_T*, SOCKET_T*, uint32_t);
 int32_t send_tcp_message(const void*, size_t, SOCKET_T);
 int32_t recv_tcp_message(void*, size_t, SOCKET_T);
-SOCKET_T accept_incoming(SOCKET_T, SOCKADDR_IN_T*);
+SOCKET_T accept_incoming(SOCKET_T, const SOCKADDR_IN_T*);
 void finalize_tcp(SOCKET_T);
 #pragma endregion
 
