629af6a99753bc6e93880fe5069a9e0d19711dcb
Keyboard Destroyer <grudko2003@gmail.com>
2025-06-09 21:13:49 +0300

    Removed alloc from cpps

--- libraries/messages/AxonMessage.cpp
+++ libraries/messages/AxonMessage.cpp
@@ -8,68 +8,20 @@ Networking::SerializedAxonMessage::SerializedAxonMessage(const AxonMessage &mess
     size(message.getSize())
 {
     const TAG_T tag = compressTag(message.getPartID(), message.getFlags(), message.ID());
-    bytes = serialize(static_cast<char*>(message.getMessage()), message.getSize(), tag, &size);
+    bytes = std::shared_ptr<char[]> ( serialize(static_cast<char*>(message.getMessage()), message.getSize(), tag, &size) );
 }
 
 Networking::SerializedAxonMessage::SerializedAxonMessage(const char* bits, size64_t actualSize) :
     size(actualSize)
 {
     if (actualSize > 0) {
-        bytes = new char[size];
-        memcpy(const_cast<char *>(bytes), bits, size);
+        bytes = std::shared_ptr<char[]> ( new char[size] );
+        memcpy(bytes.get(), bits, size);
     }
-}
-
-Networking::SerializedAxonMessage::SerializedAxonMessage(const SerializedAxonMessage &message) :
-    size(message.size),
-    offset(message.offset)
-{
-    if (size > 0)
-    {
-        bytes = new char[size];
-        memcpy(const_cast<char*>(bytes), message.bytes, size);
-    }
-}
-
-Networking::SerializedAxonMessage::SerializedAxonMessage(SerializedAxonMessage &&message) noexcept :
-        size(std::exchange(message.size, 0)),
-        bytes(std::exchange(message.bytes, nullptr))
-{}
-
-Networking::SerializedAxonMessage::~SerializedAxonMessage() {
-    if (bytes && owning)
-        delete[] bytes;
 }
 
 TAG_T Networking::SerializedAxonMessage::compressTag(uint8_t optionalData, uint8_t newFlags, uint16_t id) {
     return ((optionalData << 8) | newFlags) << 16 | id;
-}
-
-Networking::SerializedAxonMessage &
-Networking::SerializedAxonMessage::operator=(const SerializedAxonMessage &message) {
-    if (&message == this)
-        return *this;
-
-    size = message.size;
-
-    if (size > 0)
-    {
-        bytes = new char[size];
-        memcpy(const_cast<char*>(bytes), message.bytes, size);
-    }
-
-    return * this;
-}
-
-Networking::SerializedAxonMessage &
-Networking::SerializedAxonMessage::operator=(SerializedAxonMessage &&message) noexcept {
-    if (&message == this)
-        return *this;
-
-    std::exchange(size, message.size);
-    std::exchange(bytes, message.bytes);
-
-    return * this;
 }
 
 #pragma endregion
@@ -84,20 +36,25 @@ Networking::AxonMessage::AxonMessage(const void* message, size64_t size, uint8_t
 	if (message == nullptr || size == 0)
 		return;
 
-	this->message = new char[size];
-	memcpy(this->message, message, size);
+	this->message = std::shared_ptr<char[]>(new char[size]);
+	memcpy(this->message.get(), message, size);
 }
 
 Networking::AxonMessage::AxonMessage(const SerializedAxonMessage &serialized)
 {
     TAG_T tag;
+
+    void * buffer;
+
     deserialize(
-            serialized.bytes,
+            serialized.bytes.get(),
             serialized.size,
-            &this->message,
+            &buffer,
             &this->size,
             &tag
     );
+
+    message = std::shared_ptr<char[]>(static_cast<char*>(buffer));
     decompressTag(tag, &partID, &flags, &this->uniqueID);
 }
 
@@ -108,36 +65,14 @@ Networking::AxonMessage::AxonMessage(const AxonMessage &message, const uint8_t a
     addFlag(ACKNOWLEDGE);
 }
 
-Networking::AxonMessage::AxonMessage(const AxonMessage& message) :
-    size(message.size),
-    partID(message.partID),
-    flags(message.flags),
-    uniqueID(message.uniqueID)
-{
-    if (!message.getMessage() || message.size == 0)
-        return;
-
-	this->message = new char[size];
-	memcpy(this->message, message.getMessage(), this->size);
-}
-
-Networking::AxonMessage::AxonMessage(AxonMessage &message, size64_t size, uint8_t partID, uint8_t flags, uint64_t uniqueID, size64_t offset) :
+Networking::AxonMessage::AxonMessage(const AxonMessage &message, size64_t size, uint8_t partID, uint8_t flags, uint64_t uniqueID, size64_t offset) :
     size(size),
     message(message.message),
     partID(partID),
     flags(flags),
     uniqueID(uniqueID),
     offset(offset)
-{
-    message.owning = false;
-}
-
-Networking::AxonMessage::~AxonMessage()
-{
-	if (this->message && owning)
-		delete[] static_cast<char*>( message );
-    message = nullptr;
-}
+{}
 
 Networking::SerializedAxonMessage Networking::AxonMessage::getSerialized() const noexcept {
     return SerializedAxonMessage(*this);
@@ -166,23 +101,18 @@ Networking::AxonMessage::UniqueAxonMessagePtr Networking::AxonMessage::split(con
     return std::make_unique<AxonMessage>(*this, left, partID + 1, flags ^ PARTIAL, uniqueID, size);
 }
 
-void Networking::AxonMessage::append(const Networking::AxonMessage &other) {
+void Networking::AxonMessage::append(const AxonMessage &other) {
     if (!other.getMessage() || other.size == 0 || other.partID != partID + 1)
         return;
 
     char* tempBuffer = new char[other.size + size];
 
-    // assert((uintptr_t) &size < (uintptr_t) tempBuffer || (uintptr_t) &size > (uintptr_t) tempBuffer + other.size + size);
-
     if (message) {
         memcpy(tempBuffer, getMessage(), size);
-        if (owning)
-            delete[] static_cast<char*>(message);
-        message = nullptr;
     }
-    memcpy((static_cast<char*>(tempBuffer) + size), other.getMessage(), other.size);
+    memcpy(tempBuffer + size, other.getMessage(), other.size);
 
-    message = tempBuffer;
+    message = std::shared_ptr<char[]>(tempBuffer);
     size += other.size;
     partID = other.partID;
 }
--- libraries/messages/AxonMessage.hpp
+++ libraries/messages/AxonMessage.hpp
@@ -46,26 +46,18 @@ namespace Networking
     public:
     	SerializedAxonMessage() = delete;
     	SerializedAxonMessage(const char*, size64_t);
-    	SerializedAxonMessage(const SerializedAxonMessage&);
     	explicit SerializedAxonMessage(const AxonMessage&);
-    	SerializedAxonMessage(SerializedAxonMessage&&) noexcept;
 
-    	~SerializedAxonMessage();
-
-    	WGETTER( size_t getSize() ) { return size; }
-    	WGETTER ( const char* getBits() ) { return bytes; }
-
-    	SerializedAxonMessage& operator=(const SerializedAxonMessage&);
-    	SerializedAxonMessage& operator=(SerializedAxonMessage&&) noexcept;
+    	WGETTER ( size_t getSize() ) { return size; }
+    	WGETTER ( const char* getBits() ) { return bytes.get(); }
     protected:
     	static TAG_T compressTag(uint8_t, uint8_t, uint16_t);
     private:
     	size64_t    size        = 0;
     	uint16_t    uniqueID    = 0;
     	uintptr_t   offset      = 0;
-    	bool        owning      = true;     // todo: `uint16_t _refcnt;` or shared_ptr around bytes array
-    	const char* bytes       = nullptr;
 
+    	std::shared_ptr<char[]> bytes       = nullptr;
         friend class AxonMessage;
     };
 
@@ -105,18 +97,15 @@ namespace Networking
         /**
          * Copy constructor
          */
-        AxonMessage(const AxonMessage &);
-		AxonMessage(AxonMessage&, size64_t, uint8_t, uint8_t, uint64_t, size64_t);
-		~AxonMessage();
+		AxonMessage(const AxonMessage &, size64_t, uint8_t, uint8_t, uint64_t, size64_t);
 
         WGETTER(SerializedAxonMessage getSerialized());
-        WGETTER(void* getMessage()) { return (message) ? static_cast<void*>(static_cast<uint8_t*>(message) + offset) : message; }
+        WGETTER(void* getMessage()) { return (message.get()) ? static_cast<void*>(message.get() + offset) : nullptr; }
         WGETTER(size64_t getSize()) { return size; }
         WGETTER(uint16_t ID()) { return uniqueID; }
         WGETTER(uint8_t getFlags()) { return flags; }
         WGETTER(uint8_t getPartID()) { return partID; }
         WGETTER(bool hasFlag(const TAG_FLAGS flag)) { return flags & flag; }
-
         WGETTER(std::bitset<8> getFlagSet()) { return flags; }
 
         void setPartID(const uint8_t id) { this->partID = id; }
@@ -137,12 +126,12 @@ namespace Networking
         static void decompressTag(TAG_T, uint8_t*, uint8_t*, uint16_t*);
 	private:
 		size64_t	size = 0;
-		void*		message = nullptr;
-        bool        owning  = true;
         uint8_t     partID = 0;
         uint8_t     flags = UNDEFINED;
         size64_t    offset = 0;
         uint16_t    uniqueID = generateUniqueID();
+
+		std::shared_ptr<char[]> message = nullptr;
 	};
 }
 
